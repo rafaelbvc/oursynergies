2e33ff96b85cf3c3596e92af84799a467b81e18b
rafaelbvc <rafaelbvc@hotmail.com>
2024-11-16 00:03:23 -0300

    v.P-0.1.0 - Stable

--- backend/src/validations/userValidation.ts
+++ backend/src/validations/userValidation.ts
@@ -16,7 +16,7 @@ import {  body  } from "express-validator"
         body("password")        
         .exists()
         .withMessage("Password is required")
-        .matches("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,15}/)
         .withMessage("Password should be valid"),
         body("role")
         .exists()
@@ -27,24 +27,24 @@ import {  body  } from "express-validator"
     ]
 
 
-    export  const patchUserValidator = [
-        body("username")
-        .optional()
-        .isString()
-        .withMessage("User name should be string")
-        .isLength({ min: 3 })
-        .withMessage("Name must have at least 3 characters"),
-        body("password")        
-        .optional()
-        .matches("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")
-        .withMessage("Password should be valid"),
-        body("role")
-        .optional()
-        .isString()
-        .withMessage("Need to set a role")
-        .isIn(["admin","member","viewer"])
-        .withMessage("Invalid role")
-    ]
+     export  const patchUserValidator = [
+         body("username")
+         .optional()
+         .isString()
+         .withMessage("User name should be string")
+         .isLength({ min: 3 })
+         .withMessage("Name must have at least 3 characters"),
+         body("password")        
+         .optional()
+         .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,15}/)
+         .withMessage("Password should be valid"),
+         body("role")
+         .optional()
+         .isString()
+         .withMessage("Need to set a role")
+         .isIn(["admin","member","viewer"])
+         .withMessage("Invalid role")
+     ]
 
 
 
